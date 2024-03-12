b22249c0663eb650fd0c6b60f44987e4d9d0314b
Ankit Kumar <hiankit@proton.me>
2024-03-13 00:12:27 +0530

    feature: Expense List Api(in progress)

--- api/expense/urls.py
+++ api/expense/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ExpenseCreateApi, ExpenseUpdateApi, ExpenseDeleteApi
+from .views import ExpenseCreateApi, ExpenseUpdateApi, ExpenseDeleteApi, ExpenseListApi
 
 urlpatterns = [
     path("add/", ExpenseCreateApi.as_view(), name="add_expense"),
     path("<int:pk>/update/", ExpenseUpdateApi.as_view(), name="update_expense"),
     path("<int:pk>/delete/", ExpenseDeleteApi.as_view(), name="delete_expense"),
+    path("current/", ExpenseListApi.as_view(), name="list_api"),
 ]
--- api/expense/views.py
+++ api/expense/views.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import ValidationError
 from .serializers import ExpenseInputSerializer, ExpenseOutputSerializer
 from .models import Category, Expense
 from api.utils import APIResponse
@@ -127,6 +129,54 @@ class ExpenseDeleteApi(APIView):
                 status_code=status.HTTP_403_FORBIDDEN,
             )
 
+        except Exception as e:
+            return APIResponse.error(
+                str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class ExpenseListApi(APIView):
+
+    def get(self, request):
+        current_date = datetime.now()
+        current_month = current_date.month
+        current_year = current_date.year
+        category_param = request.query_params.get("category")
+        try:
+            if not category_param:
+                raise ValidationError("Category parameter is required.")
+
+            category = Category.objects.get(id=category_param)
+
+            expense = Expense.objects.filter(
+                user=request.user,
+                created_at__month=current_month,
+                created_at__year=current_year,
+                category=category,
+            )
+            res = ExpenseOutputSerializer(expense, many=True)
+            return APIResponse.success(
+                "record fetched successfully",
+                data=res.data,
+                status_code=status.HTTP_200_OK,
+            )
+        except Expense.DoesNotExist:
+            return APIResponse.error(
+                "expense not found", status_code=status.HTTP_400_BAD_REQUEST
+            )
+        except ValidationError as ve:
+            error_detail = ve.detail[0] if ve.detail else None
+            error_message = (
+                str(error_detail) if error_detail else "Validation error occurred"
+            )
+            return APIResponse.error(
+                error_message,
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
+        except Category.DoesNotExist:
+            return APIResponse.error(
+                "category not found", status_code=status.HTTP_400_BAD_REQUEST
+            )
         except Exception as e:
             return APIResponse.error(
                 str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
