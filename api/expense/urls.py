545d80d3db7eb71addf5ca556ef594baa8849fe9
Ankit Kumar <hiankit@proton.me>
2024-03-17 23:36:34 +0530

    changed the url from current to list

--- api/expense/urls.py
+++ api/expense/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("add/", ExpenseCreateApi.as_view(), name="add_expense"),
     path("<int:pk>/update/", ExpenseUpdateApi.as_view(), name="update_expense"),
     path("<int:pk>/delete/", ExpenseDeleteApi.as_view(), name="delete_expense"),
-    path("current/", ExpenseListApi.as_view(), name="list_api"),
+    path("list/", ExpenseListApi.as_view(), name="list_api"),
 ]
